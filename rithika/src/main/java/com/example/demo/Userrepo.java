7eb4670b2a7de217a32a8dc093c556c437e8d6e8
rithikabadam <94275810+rithikabadam@users.noreply.github.com>
2023-01-16 11:09:35 +0530

    Update Userrepo.java

--- rithika/src/main/java/com/example/demo/Userrepo.java
+++ rithika/src/main/java/com/example/demo/Userrepo.java
@@ -1,10 +1,15 @@
 package com.example.demo;
 
-import com.example.demo.Userrepo;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface Userrepo extends CrudRepository<User, Long> {
-    User findByEmailIdIgnoreCase(String emailId);
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ravibabu.flightaware.models.User;
+import com.ravibabu.flightaware.models.UserIp;
+
+
+public interface userRepo extends JpaRepository<User,Long> {
+
+	User findByEmailIdIgnoreCase(String email);
+	
+
 }
